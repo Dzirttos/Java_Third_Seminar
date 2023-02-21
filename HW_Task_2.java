b9026f9e60712c79c397f05ce2821a3b89354b5f
Dizrtt <demonic2003@bk.ru>
2023-02-21 20:49:33 +0200

    Homework Task № 2 started.

---
+++ HW_Task_2.java
@@ -0,0 +1,10 @@
+// 2 Пусть дан произвольный список целых чисел, удалить из него четные числа
+
+public class HW_Task_2 {
+    public static void main(String[] args) {
+    System.out.print("\033[H\033[J");
+
+
+
+    }
+}
